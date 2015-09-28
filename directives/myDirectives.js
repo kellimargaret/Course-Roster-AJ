2ba7971d80166f807355e4c8fa56907d14649937
Phillip Shannon & Kelli Sommerdyke <student@epicodus.com>
2015-09-28 10:44:49 -0700

    Add myDirectives

---
+++ directives/myDirectives.js
@@ -0,0 +1,14 @@
+courseRoster.directive("hometext", function() {
+  return {
+    template: "Back to the beginning"
+  }
+});
+
+
+courseRoster.directive("changeClass", function() {
+  return function (scope, element) {
+    element.bind("click", function() {
+      element.toggleClass("bg-danger");
+    });
+  }
+});
